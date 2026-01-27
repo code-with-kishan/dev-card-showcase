308b1220aa2acfc7672b81e32f793ee6305e79f5
omkar hole <omkarhole314@gmail.com>
2026-01-27 20:35:07 +0530

    fixed

--- js/include.js
+++ js/include.js
@@ -9,21 +9,21 @@ function loadHTML(id, file, callback) {
         .catch(error => console.log("Error loading file:", file));
     function initThemeToggle() {
         const themeToggleBtn = document.getElementById("themeToggle");
-        if(!themeToggleBtn) return;
-          const savedTheme = localStorage.getItem("theme");
-        if (savedTheme==="light") {
+        if (!themeToggleBtn) return;
+        const savedTheme = localStorage.getItem("theme");
+        if (savedTheme === "light") {
             document.body.classList.add("light-mode");
-            themeToggleBtn.textContent="🌞";
+            themeToggleBtn.textContent = "🌞";
         }
-        else{
-            themeToggleBtn.textContent="🌜";
+        else {
+            themeToggleBtn.textContent = "🌜";
         }
         themeToggleBtn.addEventListener("click", () => {
-            const isLightMode=document.body.classList.toggle("light-mode");
+            const isLightMode = document.body.classList.toggle("light-mode");
             localStorage.setItem("theme", isLightMode ? "light" : "dark");
             themeToggleBtn.textContent = isLightMode ? "☀️" : "🌙";
         });
-    }    
+    }
 }
 
 loadHTML("navbar", "navbar.html", () => {
